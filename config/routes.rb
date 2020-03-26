42a087777edbca5b31a02c0196f63cf2e0962512
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-03-26 00:16:09 +0000

    fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :needs do
     resources :services, only: [:new, :create]
   end
-  resources :services, only! [:destroy]
+  resources :services, only: [:destroy]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
