31a10d0ccdfcc4be9a8f8de208a918027e438e0d
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-03-26 00:29:51 +0000

    add schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_233817) do
+ActiveRecord::Schema.define(version: 2020_03_26_002728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_25_233817) do
     t.string "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0
     t.index ["user_id"], name: "index_needs_on_user_id"
   end
 
