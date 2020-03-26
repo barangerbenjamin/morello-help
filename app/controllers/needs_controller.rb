a9adc95e7ee8809fa73c57295f0dc750ea21ece4
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-03-26 00:24:15 +0000

    add new/create for need

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -3,5 +3,25 @@ class NeedsController < ApplicationController
     def index
         @needs = Need.all
     end
+
+    def new
+        @need = Need.new
+    end
+
+    def create
+        @need = Need.new(need_params)
+        @need.user = current_user
+        if @need.save
+            redirect_to need_path(@need)
+        else
+            render :new
+        end
+    end
+    
+    private
+
+    def need_params
+       params.require(:need).permit(:name, :note) 
+    end
     
 end
