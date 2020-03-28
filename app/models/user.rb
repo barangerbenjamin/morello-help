02272fb9a185d87bf3ceba8094e3e73e4662188c
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-03-28 11:40:06 +0000

    add first name & last name to users + vallidations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ApplicationRecord
   has_many :needs
   has_many :services
   has_one_attached :photo
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :complex, presence: true, inclusion: [ "Amarelle", "Maraschino", "Rainier", "Santina" ]
   validates :flat_number, presence: true, numericality: { only_integer: true }
+
+  def full_name
+    first_name.capitalize + " " + last_name.capitalize
+  end
+  
 end
