a5a8f737515d5900fb214daf1f83b0f517c94b94
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-03-30 00:50:41 +0100

    fix indentation

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,33 +1,33 @@
 class NeedsController < ApplicationController
     before_action :set_need, only: [:show]
     def index
-        @needs = Need.all
+      @needs = Need.all
     end
 
     def show; end
 
     def new
-        @need = Need.new
+      @need = Need.new
     end
 
     def create
-        @need = Need.new(need_params)
-        @need.user = current_user
-        if @need.save
-            redirect_to need_path(@need)
-        else
-            render :new
-        end
+      @need = Need.new(need_params)
+      @need.user = current_user
+      if @need.save
+        redirect_to need_path(@need)
+      else
+        render :new
+      end
     end
     
     private
 
     def set_need
-        @need = Need.find(params[:id])
+      @need = Need.find(params[:id])
     end
 
     def need_params
-       params.require(:need).permit(:name, :note, :quantity, :category) 
+      params.require(:need).permit(:name, :note, :quantity, :category) 
     end
     
 end
