67464acf5acc42f15df15fec3b10b00436070415
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-03-29 20:54:57 +0100

    add validation on need name

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,6 +1,6 @@
 class Need < ApplicationRecord
   belongs_to :user
   has_many :services
-
+  validates :name, length: { in: 3..20 }
   validates :quantity, numericality: { only_integer: true }
 end
