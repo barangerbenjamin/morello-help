bfdc5d937388ab534770ad41c808cc2a4c4761e1
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-03-30 16:11:00 +0100

    change style for form + refine flatpickr

--- app/javascript/packs/components/init_flatpickr.js
+++ app/javascript/packs/components/init_flatpickr.js
@@ -1,5 +1,5 @@
 import flatpickr from "flatpickr"
-import 'flatpickr/dist/themes/dark.css'
+import 'flatpickr/dist/themes/airbnb.css'
 
 const init_flatpickr = () => {
   flatpickr(".flatpickr", {
@@ -7,7 +7,16 @@ const init_flatpickr = () => {
     enableTime: true,
     minDate: "today",
     maxDate: new Date().fp_incr(14),
-    time_24hr: true
+    time_24hr: true,
+    shorthandCurrentMonth: true,
+    defautlDate: new Date,
+    allowInput: true,
+    altInput: true,
+    "locale": {
+      "firstDayOfWeek": 1,
+      "startDate": new Date,
+      "todayHighlight": true
+    }
   });
 }
 
