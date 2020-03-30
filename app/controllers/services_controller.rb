01c10ca0e6560f1ae67ae15e0b1b0150fbdba512
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-03-30 18:50:03 +0100

    fix color select option

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,7 @@
 class ServicesController < ApplicationController
   def create
     @need = Need.find(params[:need_id])
-    @need.services.build_user(user: current_user)
+    @service = @need.services.build(user: current_user)
     if @service.save
       redirect_to need_path(@need), notice: "You are helping #{@need.user.full_name} out!"
     else
