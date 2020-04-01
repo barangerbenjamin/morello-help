cba3f5aef313682e9d2f47d7890633cee67857f9
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-04-01 17:31:09 +0100

    add sweetalert2 for validation

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,9 +1,26 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
+import Swal from 'sweetalert2';
 export default class extends Controller {
   static targets = ["submitLink"];
 
   clickSubmit() {
     const slt = this.submitLinkTarget
     slt.click();
+  }
+
+  confirm(event) {
+    Swal.fire({
+      title: `<p class="text-dark font-weight-bolder">All done?</p>`,
+      html: `<p class="text-dark font-weight-bolder">You're about to confirme that you have helped ${this.submitLinkTarget.dataset.needuser}</p>`,
+      type: 'warning',
+      footer: `<p class="text-dark font-weight-bolder">You're the boss <i class="fas fa-crown""></i></p>`,
+      confirmButtonColor: '#1A1A1A',
+      confirmButtonText: 'Confirm   <i class="fas fa-check"></i>',
+      showCancelButton: true
+    }).then((result) => {
+      if (result.value) {
+        this.submitLinkTarget.click();
+      }
+    });
   }
 }
